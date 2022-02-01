1de454f275434879a6ac5d1d1e2cff7e2e3e7cf7
Rostyslav997 <ronasobkiv@gmail.com>
2022-02-01 13:13:43 +0200

    lesson1, homework

---
+++ JS/lesson1/lesson1_hw.js
@@ -0,0 +1,104 @@
+let owu = 'owu'
+let hello = 'hello'
+let com = 'com'
+let ua = 'ua'
+let one = 1
+let ten = 10
+let nine = -999
+let one123 = 123
+let pi = 3.14
+let two7 = 2.17
+let sixteen = 16
+let true1 = 5>4
+let false1 = 5>6
+console.log(owu,hello,com,ua,one,ten,nine,one123,pi,two7,sixteen,true1,false1)
+//alert(owu)
+//alert(hello)
+//alert(com)
+//alert(ua)
+//alert(one)
+//alert(ten)
+//alert(nine)
+//alert(one123)
+//alert(pi)
+//alert(two7)
+//alert(sixteen)
+//alert(true1)
+//alert(false1)
+document.write(owu,` `,hello,` `,com,` `,ua,` `,one,` `,ten,` `,nine,` `,one123,` `,pi,` `,two7,` `,sixteen,` `,true1,` `,false1,` `)
+owu = 'okten'
+hello = 'hi'
+com = 'dot'
+ua = 'Ukraine'
+one = 1.0
+ten = 'ten'
+nine = 999
+one123 = 123
+pi = 3.1415
+two7 = 27
+sixteen = 'sixteen'
+true1 = 6>4
+false1 = 4>6
+console.log(owu,hello,com,ua,one,ten,nine,one123,pi,two7,sixteen,true1,false1)
+//alert(owu)
+//alert(hello)
+//alert(com)
+//alert(ua)
+//alert(one)
+//alert(ten)
+//alert(nine)
+//alert(one123)
+//alert(pi)
+//alert(two7)
+//alert(sixteen)
+//alert(true1)
+//alert(false1)
+document.write(owu,` `,hello,` `,com,` `,ua,` `,one,` `,ten,` `,nine,` `,one123,` `,pi,` `,two7,` `,sixteen,` `,true1,` `,false1,` `)
+let firsName = 'Rostyslav '
+let middleName = 'Vasylovuch '
+let lastName = 'Sobkiv '
+let person = firsName+middleName+lastName
+console.log(person)
+//let age = prompt('Your age')
+//let firstName = prompt('Your fist Name')
+//let lastName = prompt('Your last Name')
+//console.log('Hello',firstName,lastName,'Your age is', age )
+
+let a = 100
+let b = '100'
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+let q = 5<6
+console.log(q)
+q = 5>6
+console.log(q)
+q = 5==6
+console.log(q)
+q = 5>=6
+console.log(q)
+
+let w = 10==10
+console.log(w)
+w = 10>=10
+console.log(w)
+w = 10<10
+console.log(w)
+w = 10!==10
+console.log(w)
+w = 10>10
+console.log(w)
+
+one123 = 123>'123'
+console.log(one123)
+one123 = 123=='123'
+console.log(one123)
+
+let str = "20";
+let a1 = 5;
+document.write(str + a1 + "<br/>"); //Тому що, тут число 5, додається до стрічкового значення 20
+document.write(str - a1 + "<br/>")//Тому що, при відніманні, число 5, віднімається від стрічкового значення числа 20
+document.write(str * "2" + "<br/>")//Тому що, значення 20, примножується на значення 2
+document.write(str / 2 + "<br/>")//Тому що, значення 20, ділиться на 2
